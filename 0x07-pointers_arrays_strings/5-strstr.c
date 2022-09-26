7b66e173140e035a1c80730236eaf3e227320940
Emyx3D <epsilocybin@gmail.com>
2022-09-26 13:08:33 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strstr - finds the first occurrence of the substring
+ * needle in the string
+ * @haystack: entire string
+ * @needle: substring
+ * Return: pointer to the begining of located substrng 
+ * NULL if the substring is not found
+ */
+char *strstr(char *haystack, char *needle)
+{
+	char *rhaystack;
+	char *qneedle;
+
+	while (*haystack != '\0')
+	{
+		rhaystack = haystack;
+		qneedle = needle;
+
+		while (*haystack != '\0' && *qneedle != '\0' && *haystack == *qneedle)
+		{
+			haystack++;
+			qneedle++;
+		}
+		if (!*qneedle)
+			return (rhaystack);
+		haystack = rhaystack + 1;
+	}
+	return (0);
+}
