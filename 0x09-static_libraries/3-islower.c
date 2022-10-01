7f0066ad397c7270696aeaa318e4276f56efda2f
Emyx3D <epsilocybin@gmail.com>
2022-10-01 01:25:57 -0700

    libmy

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check for
+ *
+ * Return 1 if c is lowercase, otherwise return 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
